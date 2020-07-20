c38c4b5c3aa794b29bb450ed139f8095a23b336a
ultimategamer98 <forgedcircuitultimategamer@hotmail.com>
2020-07-20 11:19:45 -0400

    add code mid video

---
+++ 02_Variables/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x = 0;
+	int cars = 14;
+	int debt = -4000;
+	float cash = 2.32;
+	double credit = 32.32;
+	char a = 'a';
+	char* sandwich = "ham";
+	bool do_i_like_coffee = true;
+	auto are_we_there_yet = false;
+	auto blank_check = 200.00;
+	int over_9000 = INT_MAX;
+
+	cout << over_9000 << endl;
+	int _ = 5;
+	cout << _ << endl;
+
+
+		/*std::cout << "Ham Sandwich!\n";
+		return 0;*/
+		return 0;
+}
